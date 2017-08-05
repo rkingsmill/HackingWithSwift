8e9a7ae69464b9acf7e4f1583fa85fd0958ae398
RKing <rosalyn.kingsmill@gmail.com>
2017-08-05 15:18:33 -0400

    project 7 completed

--- Project7/AppDelegate.swift
+++ Project7/AppDelegate.swift
@@ -15,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
+            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            tabBarController.viewControllers?.append(vc)
+        }
         // Override point for customization after application launch.
         return true
     }
